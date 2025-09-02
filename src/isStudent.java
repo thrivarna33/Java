cc2c27327c3a2de11514d55386f5404728617113
Thrivarna Donepalli <thrivarnavenkateshwarlu@gmail.com>
2025-09-02 12:10:08 +0530

    Java basics

---
+++ src/isStudent.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class isStudent {
+public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Are you a student?");
+    String answer=sc.nextLine();
+    if(answer.equalsIgnoreCase("yYESes"))
+    {
+        System.out.println("You are a student!");
+    }
+    else {
+        System.out.println("you are not a student!");
+    }
+}
+}
